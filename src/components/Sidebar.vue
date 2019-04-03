14e1617db479a9e5269e3cfee14a62e69a12d840
Leonardo Gonzalez Gonzalez <leonardogonzalez201190@gmail.com>
2019-04-03 11:25:31 -0400

    Adjusting lines and sizes.

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,20 +1,22 @@
 <template>
   <div id="wrapper">
-  <div id="sidebar-wrapper">
-            <div class="sidebar-nav">
-                <li class="sidebar-brand" style="margin:10px">
-                    <a href="#">
-                        <h2>TEK<strong>TASK</strong></h2>
-                    </a>
-                </li>  
-	<draggable v-model="items" group="itemsSidebar" @start="drag=true" @end="drag=false">
-	<div v-for="element in items" :key="element.title">
-		<SessionSidebar :title="element.title" :iconTitle="element.iconTitle" :iconItem="element.iconItem" />
-	</div>
-	</draggable>
-	</div>
-	</div>
-		</div>
+    <div id="sidebar-wrapper">
+      <div class="sidebar-nav">
+          <li class="sidebar-brand brandup" style="margin:10px">
+              <a href="#">
+                  <h2>TEK<strong>TASK</strong></h2>
+              </a>
+          </li> 
+    <draggable v-model="items" group="itemsSidebar" @start="drag=true" @end="drag=false">
+	    <div v-for="element in items" :key="element.title">
+        <hr>
+          <SessionSidebar :title="element.title" :iconTitle="element.iconTitle" :iconItem="element.iconItem" />
+        <hr>
+      </div>
+  </draggable>
+	 </div>
+	      </div>
+		          </div>
 </template>
 
 <script>
@@ -29,7 +31,7 @@ export default {
   },
   data() {
 	return {
-		items:[{title:"My Projects", iconTitle:"fa fa-pager", iconItem:"fa fa-star"},
+		items:[{title:"My Projects", iconTitle:"fa fa-credit-card", iconItem:"fa fa-star"},
 		{title:"Team", iconTitle:"fa fa-users", iconItem:"fa fa-user-friends"}]
 	}
   },
