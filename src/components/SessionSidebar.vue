8c166288fd7ff509f450daf27bd48007d8e640de
Leonardo Gonzalez Gonzalez <leonardogonzalez201190@gmail.com>
2019-04-04 14:24:52 -0400

    Fixing styles.

--- src/components/SessionSidebar.vue
+++ src/components/SessionSidebar.vue
@@ -1,15 +1,15 @@
 <template>
   <small>
-    <hr style="background: #374051;">
+    <hr class="th_sparetor">
       <p class="text-white cursor-move font_ text-justify ml-5 h6">
         <span class="text-secondary" :class="iconTitle"></span>
        {{title}}</p>
-    <hr style="background: #374051;">
+    <hr class="th_sparetor">
 
     <input class="form-control form-control-sm" type="text" v-if="showInput" @keyup.13="increment" v-model="item_text" />
     <draggable v-model="items" @group="title" @start="drag=true" @end="drag=false">
       <div v-for="element in items" :key="element" class="item_session">
-        <a class="font_ text-justify mt-1 ml-5"><span :class="iconItem" class="text-secondary"></span>
+        <a class="font_ text-justify mt-1 ml-5"><span :class="iconItem" class="text-secondary mb-2 mt-2"></span>
          <span class="text-white"> {{element}}</span>
         </a>
       </div>
@@ -18,6 +18,7 @@
     <button class="btn btn-outline-primary btn-sm mt-1 font_ text-justify ml-5" @click="showForm" :disabled="showInput">
       <span class="fa fa-plus"></span> ADD {{title}}
     </button>
+    <br class="mb-4">
   </small>
 </template>
 
