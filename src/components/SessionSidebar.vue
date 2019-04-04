1c0d79599368477c3417542f6a40656544c45e88
Leonardo Gonzalez Gonzalez <leonardogonzalez201190@gmail.com>
2019-04-04 09:22:08 -0400

    Fixing styles.

--- src/components/SessionSidebar.vue
+++ src/components/SessionSidebar.vue
@@ -1,19 +1,19 @@
 <template>
   <small>
+    <hr style="background: #374051;">
+      <h5 class="text-white cursor-move font_ text-justify ml-5"><span :class="iconTitle"> {{title}}</span></h5>
+    <hr style="background: #374051;">
 
-    <h5 class="text-white cursor-move font_"><span :class="iconTitle"> {{title}}</span></h5>
-
-			<input class="form-control form-control-sm sm" type="text" v-if="showInput" @keyup.13="increment" v-model="item_text" />
-			<draggable v-model="items" @group="title" @start="drag=true" @end="drag=false">
-				<div v-for="element in items" :key="element" class="item_session">
-					<a class="text-white font_ text-justify mt-1 ml-5"><span :class="iconItem"></span> {{element}}</a>
-				</div>
-			</draggable>
-	<p v-if="!items.length" class="text-secondary ">Empty list</p>
-	 <button class="btn btn-outline-primary btn-sm mt-1" @click="showForm" :disabled="showInput">
-	 <span class="fa fa-plus"></span> ADD {{title}}
-	 </button>
-
+    <input class="form-control form-control-sm sm" type="text" v-if="showInput" @keyup.13="increment" v-model="item_text" />
+    <draggable v-model="items" @group="title" @start="drag=true" @end="drag=false">
+      <div v-for="element in items" :key="element" class="item_session">
+        <a class="text-white font_ text-justify mt-1 ml-5"><span :class="iconItem"></span> {{element}}</a>
+      </div>
+    </draggable>
+    <p v-if="!items.length" class="text-secondary font_ text-justify ml-5">Empty list</p>
+    <button class="btn btn-outline-primary btn-sm mt-1 font_ text-justify ml-5" @click="showForm" :disabled="showInput">
+      <span class="fa fa-plus"></span> ADD {{title}}
+    </button>
   </small>
 </template>
 
@@ -23,29 +23,29 @@ import draggable from 'vuedraggable'
 export default {
   name: 'SessionSidebar',
   components: {
-    draggable
-  },
+      draggable
+    },
   data() {
-	return {
-		items:[],
-		showInput: false,
-		item_text:''
-	}
+    return {
+      items:[],
+      showInput: false,
+      item_text:''
+      }
   },
   props: {
     title: String,
-	iconTitle: String,
-	iconItem: String
+    iconTitle: String,
+    iconItem: String
   },
   methods: {
     showForm (){
-		this.showInput = true
-	},
-	increment (){
-		this.showInput = false
-		this.items.push(this.item_text)
-		this.item_text = ''
-	}
+        this.showInput = true
+    },
+    increment (){
+      this.showInput = false
+      this.items.push(this.item_text)
+      this.item_text = ''
+    }
   }
 }
 </script>
--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -2,19 +2,19 @@
   <div id="wrapper">
     <div id="sidebar-wrapper">
       <div class="sidebar-nav">
-          <li class="sidebar-brand brandup">
-              <a href="#">
-                  <h2 class="text-center">TEK<strong>TASK</strong></h2>
-              </a>
-          </li>
-    <draggable v-model="items" group="itemsSidebar" @start="drag=true" @end="drag=false">
-	    <div v-for="element in items" :key="element.title" class="div_session text-center">
-          <hr><SessionSidebar :title="element.title" :iconTitle="element.iconTitle" :iconItem="element.iconItem" />
+        <li class="sidebar-brand brandup text-center">
+          <a href="#">
+            <h2><strong>TEK</strong>TASK</h2>
+          </a>
+        </li>
+        <draggable v-model="items" group="itemsSidebar" @start="drag=true" @end="drag=false">
+          <div v-for="element in items" :key="element.title" class="div_session ">
+            <SessionSidebar :title="element.title" :iconTitle="element.iconTitle" :iconItem="element.iconItem" />
+          </div>
+        </draggable>
       </div>
-  </draggable>
-	 </div>
-	      </div>
-		          </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -25,13 +25,13 @@ export default {
   name: 'Sidebar',
   components: {
     SessionSidebar,
-	draggable
-  },
+    draggable
+    },
   data() {
-	return {
-		items:[{title:"My Projects", iconTitle:"fa fa-credit-card", iconItem:"fa fa-star"},
-		{title:"Team", iconTitle:"fa fa-users", iconItem:"fa fa-user-friends"}]
-	}
-  },
-}
+    return {
+      items:[{title:"My Projects", iconTitle:"fa fa-credit-card", iconItem:"fa fa-star"},
+             {title:"Team", iconTitle:"fa fa-users", iconItem:"fa fa-user-friends"}]
+            }
+         },
+  }
 </script>
